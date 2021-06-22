705e14a393eab220b94f352f4b1995a27a4b6ace
Savino-M <saviomarzano@gmail.com>
2021-06-22 14:40:35 +0200

    aggiunto classificatore

--- src/trainer estimation/Main.py
+++ src/trainer estimation/Main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9da0155d65eb2b5f81713691c2ba32b42c2c2f69acfa18be9b8bddd6abcea46c
-size 1634
+oid sha256:31208d044a1418dab1a0771bc7c7dfae5a797a13553d4a6fd05a5abc6338f406
+size 1670
